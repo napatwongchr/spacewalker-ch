d48ed6badf8031f9c19c7bd6f5514c862bc4d2a3
Napat <napat.wongchr@gmail.com>
2019-12-25 21:48:15 +0700

    fix: desc text at drop area should depend on devices

--- src/components/ragnarok-tactics/MonsterLineUp.js
+++ src/components/ragnarok-tactics/MonsterLineUp.js
@@ -117,11 +117,7 @@ function MonsterLineUp({ monsterList, setMonsterList }) {
           )
         })
       ) : (
-        <div css={styles.noCardInfoBox}>
-          <span>Double click monster</span>
-          <span>or</span>
-          <span>Drag monster here !</span>
-        </div>
+        <NoCardInfoBox />
       )}
 
       <Modal show={show} onHide={handleClose} centered>
@@ -200,6 +196,32 @@ const Option = props => {
           </Fragment>
         ) : null}
       </div>
+    </div>
+  )
+}
+
+function NoCardInfoBox() {
+  let windowWidth = Math.max(
+    document.documentElement.clientWidth,
+    window.innerWidth || 0
+  )
+  if (
+    windowWidth <= 576 ||
+    (windowWidth > 576 && windowWidth <= 768) ||
+    (windowWidth > 768 && windowWidth <= 992) ||
+    (windowWidth > 992 && windowWidth <= 1200)
+  ) {
+    return (
+      <div css={styles.noCardInfoBox}>
+        <span>Tap monster in hero list</span>
+      </div>
+    )
+  }
+  return (
+    <div css={styles.noCardInfoBox}>
+      <span>Double click monster</span>
+      <span>or</span>
+      <span>Drag monster here !</span>
     </div>
   )
 }
